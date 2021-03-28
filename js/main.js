4c3d40f538a26060f41c26fa42b972801e8f909c
kirillzenzura <kirillzenzura@gmail.com>
2021-03-28 18:47:27 +0300

    Making header

---
+++ js/main.js
@@ -0,0 +1,15 @@
+class MenuList {
+    constructor() {
+        this.menuToggle();
+    }
+    menuToggle() {
+        let menu = document.querySelector(".header__navigation");
+        console.log(menu)
+        let btnMenu = document.querySelector("#header-btn");
+        btnMenu.addEventListener("click", () => {
+            menu.classList.toggle("visually-hidden")
+        })
+    }
+}
+
+const menuListener = new MenuList;
